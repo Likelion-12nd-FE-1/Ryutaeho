2cbe26407cf532f72745608f5f298e449cc6e133
rtaeho <ryou0920@naver.com>
2024-05-23 09:25:13 +0900

    feat : 기본 틀 구상

--- hw2/src/App.jsx
+++ hw2/src/App.jsx
@@ -1,31 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import Home from "./Home";
+import Signup from "./Signup";
 
-const Login = () => {
+function App() {
   return (
-    <div className="login-container">
-      <div className="login-box">
-        <h2>네이버 로그인</h2>
-        <form>
-          <div className="input-group">
-            <label htmlFor="username">아이디</label>
-            <input type="text" id="username" name="username" />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">비밀번호</label>
-            <input type="password" id="password" name="password" />
-          </div>
-          <button type="submit" className="login-button">
-            로그인
-          </button>
-        </form>
-        <div className="links">
-          <a href="#">아이디 찾기</a>
-          <a href="#">비밀번호 찾기</a>
-          <a href="#">회원가입</a>
-        </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
-};
+}
 
-export default Login;
+export default App;
